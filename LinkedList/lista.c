3c92e1acc554870d3e362e82cf4867a59637bc76
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-15 19:24:47 +0300

    lista.c

--- LinkedList/lista.c
+++ LinkedList/lista.c
@@ -9,21 +9,20 @@ Lista *lista_new(void) {
   return L;
 }
 
-void lista_add(Lista *L, float val)
-{
+void lista_add(Lista *L, float val) {
   if (L->inicio) {
-      Nodo *N = NULL;
-      N = L->inicio;
-      while (N->next) {
-          N = N->next;
-      }
-      N->next = (Nodo *) malloc(sizeof(Nodo));
-      N->next->val = val;
-      N->next->next = NULL;
+    Nodo *N = NULL;
+    N = L->inicio;
+    while (N->next) {
+      N = N->next;
+    }
+    N->next = (Nodo *)malloc(sizeof(Nodo));
+    N->next->val = val;
+    N->next->next = NULL;
   } else {
-      L->inicio = (Nodo *) malloc(sizeof(Nodo));
-      L->inicio->val = val;
-      L->inicio->next = NULL;
+    L->inicio = (Nodo *)malloc(sizeof(Nodo));
+    L->inicio->val = val;
+    L->inicio->next = NULL;
   }
 }
 
