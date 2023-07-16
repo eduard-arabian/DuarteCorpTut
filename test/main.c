60e46891f9c87c0c6b9b70d78cc1aadf2eaac1fd
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-16 23:51:58 +0300

    dict test back

--- test/main.c
+++ test/main.c
@@ -2,42 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
-{
-    char *key;
-    char *value;
+typedef struct {
+  char *key;
+  char *value;
 } Pair;
 
-typedef struct
-{
-    Pair *pairs;
-    int size;
+typedef struct {
+  Pair *pairs;
+  int size;
 } Dict;
 
-int main()
-{
-    Dict *D = NULL;
-    D = malloc(sizeof(Dict));
-    D->size = 0;
-    D->pairs = NULL;
-    D->size = 1;
+int main() {
+  Dict *D = NULL;
+  D = malloc(sizeof(Dict));
+  D->size = 0;
+  D->pairs = NULL;
+  D->size = 1;
 
-    D->pairs = malloc(sizeof(Pair) * D->size);
-    char *key = "FreeBSD";
-    D->pairs[0].key = malloc(sizeof(char) * strlen(key) + 1);
-    strcpy(D->pairs[D->size - 1].key, key);
-    char *value = "Derived from Unix";
-    D->pairs[0].value = malloc(sizeof(char) * strlen(value) + 1);
-    strcpy(D->pairs[D->size - 1].value, value);
+  D->pairs = malloc(sizeof(Pair) * D->size);
+  char *key = "FreeBSD";
+  D->pairs[0].key = malloc(sizeof(char) * strlen(key) + 1);
+  strcpy(D->pairs[D->size - 1].key, key);
+  char *value = "Derived from Unix";
+  D->pairs[0].value = malloc(sizeof(char) * strlen(value) + 1);
+  strcpy(D->pairs[D->size - 1].value, value);
 
-    printf("%s: %s\n", D->pairs[0].key, D->pairs[0].value);
-    free(D->pairs[0].key);
-    D->pairs[0].key = NULL;
-    free(D->pairs[0].value);
-    D->pairs[0].value = NULL;
-    free(D->pairs);
-    D->pairs = NULL;
-    free(D);
-    D = NULL;
-    return 0;
+  printf("%s: %s\n", D->pairs[0].key, D->pairs[0].value);
+  free(D->pairs[0].key);
+  D->pairs[0].key = NULL;
+  free(D->pairs[0].value);
+  D->pairs[0].value = NULL;
+  free(D->pairs);
+  D->pairs = NULL;
+  free(D);
+  D = NULL;
+  return 0;
 }
