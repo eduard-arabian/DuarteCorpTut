8150d4de24b80b1e7b84d25c3c0f2d82bb881f5e
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-17 21:34:07 +0300

    strcpy rulez

--- Dictionaries/diccionario.c
+++ Dictionaries/diccionario.c
@@ -4,54 +4,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-Dict *dict_new(void)
-{
-    Dict *D = NULL;
-    D = malloc(sizeof(Dict));
-    if (D) {
-        D->pairs = NULL;
-        D->size = 0;
-    }
-    return D;
+Dict *dict_new(void) {
+  Dict *D = NULL;
+  D = malloc(sizeof(Dict));
+  if (D) {
+    D->pairs = NULL;
+    D->size = 0;
+  }
+  return D;
 }
 
-void dict_increase(Dict *D, int size)
-{
-    if (D) {
-        D->size += size;
-        D->pairs = realloc(D->pairs, sizeof(Pair) * D->size);
-    }
+void dict_increase(Dict *D, int size) {
+  if (D) {
+    D->size += size;
+    D->pairs = realloc(D->pairs, sizeof(Pair) * D->size);
+  }
 }
 
-void dict_add(Dict *D, const char *key, const char *value)
-{
-    if (D) {
-        dict_increase(D, 1);
-        D->pairs[D->size - 1].key = malloc(sizeof(char) * strlen(key) + 1);
-        strcpy(D->pairs[D->size - 1].key, key);
-        D->pairs[D->size - 1].value = malloc(sizeof(char) * strlen(value) + 1);
-        strcpy(D->pairs[D->size - 1].value, value);
-    }
+void dict_add(Dict *D, const char *key, const char *value) {
+  if (D) {
+    dict_increase(D, 1);
+    D->pairs[D->size - 1].key = malloc(sizeof(char) * strlen(key) + 1);
+    strcpy(D->pairs[D->size - 1].key, key);
+    D->pairs[D->size - 1].value = malloc(sizeof(char) * strlen(value) + 1);
+    strcpy(D->pairs[D->size - 1].value, value);
+  }
 }
 
-void dict_print(Dict *D)
-{
+void dict_print(Dict *D) {
+  for (int i = 0; i < D->size; ++i) {
+    printf("%s: %s\n", D->pairs[i].key, D->pairs[i].value);
+  }
+}
+
+void dict_free(Dict *D) {
+  if (D) {
     for (int i = 0; i < D->size; ++i) {
-        printf("%s: %s\n", D->pairs[i].key, D->pairs[i].value);
+      free(D->pairs[i].key);
+      D->pairs[i].key = NULL;
+      free(D->pairs[i].value);
+      D->pairs[i].value = NULL;
     }
-}
-
-void dict_free(Dict *D)
-{
-    if (D) {
-        for (int i = 0; i < D->size; ++i) {
-            free(D->pairs[i].key);
-            D->pairs[i].key = NULL;
-            free(D->pairs[i].value);
-            D->pairs[i].value = NULL;
-        }
-        free(D->pairs);
-        D->pairs = NULL;
-        free(D);
-    }
+    free(D->pairs);
+    D->pairs = NULL;
+    free(D);
+  }
 }
