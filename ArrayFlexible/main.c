efd4235cdf559632a50146df7a8baec7362f2c05
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-18 17:41:18 +0300

    Array Flexible corrected

--- ArrayFlexible/main.c
+++ ArrayFlexible/main.c
@@ -11,26 +11,32 @@ int main()
 {
     int size = 10;
     ArrFlex *T = malloc(sizeof(ArrFlex) + size * sizeof(int));
-    T->len = size;
-    for (int i = 0; i < T->len; ++i) {
-        T->array[i] = i;
-        printf("%d\n", T->array[i]);
+    if (T) {
+        T->len = size;
+        for (int i = 0; i < T->len; ++i) {
+            T->array[i] = i;
+            printf("%d\n", T->array[i]);
+        }
     }
 
     size = size * 2;
     T = realloc(T, sizeof(ArrFlex) + size * sizeof(int));
-    T->len = size;
-    for (int i = T->len / 2; i < T->len; ++i) {
-        T->array[i] = i;
+    if (T) {
+        T->len = size;
+        for (int i = T->len / 2; i < T->len; ++i) {
+            T->array[i] = i;
+        }
+
+        puts("");
+        for (int i = 0; i < T->len; ++i) {
+            printf("%d\n", T->array[i]);
+        }
     }
 
-    puts("");
-    for (int i = 0; i < T->len; ++i) {
-        printf("%d\n", T->array[i]);
+    if (T) {
+        free(T);
+        T = NULL;
     }
-
-    free(T);
-    T = NULL;
 
     return 0;
 }
