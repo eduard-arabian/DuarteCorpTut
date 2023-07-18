83c8cd36ed5708fee911e36a301e488b6c7cb88a
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-18 13:23:50 +0300

    Array Flexible

---
+++ ArrayFlexible/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int len;
+    int array[];
+} ArrFlex;
+
+int main()
+{
+    int size = 10;
+    ArrFlex *T = malloc(sizeof(ArrFlex) + size * sizeof(int));
+    T->len = size;
+    for (int i = 0; i < T->len; ++i) {
+        T->array[i] = i;
+        printf("%d\n", T->array[i]);
+    }
+
+    size = size * 2;
+    T = realloc(T, sizeof(ArrFlex) + size * sizeof(int));
+    T->len = size;
+    for (int i = T->len / 2; i < T->len; ++i) {
+        T->array[i] = i;
+    }
+
+    puts("");
+    for (int i = 0; i < T->len; ++i) {
+        printf("%d\n", T->array[i]);
+    }
+
+    free(T);
+    T = NULL;
+
+    return 0;
+}
