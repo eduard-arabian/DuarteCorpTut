6515712a6b73caf8b40290b83c7dc7d79a936233
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-20 16:43:25 +0300

    SDL2

--- JuegoNave/main.c
+++ JuegoNave/main.c
@@ -7,5 +7,15 @@
 
 int main(void)
 {
-    return 0;
+    int exit_code = EXIT_SUCCESS;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", SDL_GetError(), NULL);
+        SDL_Quit();
+        exit_code = EXIT_FAILURE;
+    } else {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "OK", "SDL started", NULL);
+    }
+
+    return exit_code;
 }
