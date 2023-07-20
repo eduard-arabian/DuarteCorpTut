8c0196c1ed586c693eecb5ba16dd50bfc9360f4e
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-20 20:16:38 +0300

    Game Ship

--- JuegoNave/main.c
+++ JuegoNave/main.c
@@ -9,7 +9,9 @@
 #define YSIZE 600
 #define MS 10
 #define SHIP_LEN 20
-#define SHIP_VEL 5
+#define SHIP_VEL 10
+#define MIS_LEN 5
+#define MIS_VEL 5
 
 typedef struct Ship Ship;
 typedef struct Missile Missile;
@@ -28,11 +30,15 @@ struct Missile
     int x1, y1;
     int x2, y2;
     int vx, vy;
+    Missile *next;
 };
 
 void ShipDraw(Ship *ship, SDL_Renderer *renderer);
 void ShipGoLeftRight(Ship *ship);
 void ShipGoUpDown(Ship *ship);
+void ShipFire(Ship *ship);
+void MissileFired(Missile *missile);
+void MissileFliedAway(Missile *missiles);
 
 int main(void)
 {
@@ -80,7 +86,7 @@ int main(void)
                 gameOver = 1;
             } else if (typeEvent == SDL_MOUSEBUTTONDOWN) {
                 if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)) {
-                    //gameOver = 1;
+                    ShipFire(&ship);
                 }
             } else if (typeEvent == SDL_KEYDOWN) {
                 if (keys[SDL_SCANCODE_ESCAPE]) {
@@ -98,9 +104,11 @@ int main(void)
                     ship.vy = abs(ship.vy);
                     ShipGoUpDown(&ship);
                 } else if (keys[SDL_SCANCODE_SPACE]) {
+                    ShipFire(&ship);
                 }
             }
         }
+        MissileFliedAway(ship.missiles);
         // screen
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_RenderClear(renderer);
@@ -119,6 +127,15 @@ int main(void)
 
 void ShipDraw(Ship *ship, SDL_Renderer *renderer)
 {
+    // draw missiles
+    Missile *ixMissile = ship->missiles;
+    while (ixMissile) {
+        MissileFired(ixMissile);
+        SDL_RenderDrawLine(renderer, ixMissile->x1, ixMissile->y1, ixMissile->x2, ixMissile->y2);
+        ixMissile = ixMissile->next;
+    }
+
+    // draw ship
     SDL_Point points[4] = {{ship->x1, ship->y1}, {ship->x2, ship->y2}, {ship->x3, ship->y3}};
     points[3] = points[0];
     SDL_RenderDrawLines(renderer, points, 4);
@@ -136,4 +153,57 @@ void ShipGoUpDown(Ship *ship)
     ship->y1 += ship->vy;
     ship->y2 += ship->vy;
     ship->y3 += ship->vy;
+}
+
+void ShipFire(Ship *ship)
+{
+    if (ship->missiles) {
+        Missile *ixMissile = ship->missiles;
+        while (ixMissile->next != NULL) {
+            ixMissile = ixMissile->next;
+        }
+        ixMissile->next = (Missile *) SDL_malloc(sizeof(Missile));
+        ixMissile = ixMissile->next;
+        ixMissile->x1 = ship->x1;
+        ixMissile->y1 = ship->y1;
+        ixMissile->x2 = ship->x1;
+        ixMissile->y2 = ship->y1 - MIS_LEN;
+        ixMissile->vx = 0;
+        ixMissile->vy = -MIS_VEL;
+        ixMissile->next = NULL;
+    } else {
+        ship->missiles = (Missile *) SDL_malloc(sizeof(Missile));
+        ship->missiles->x1 = ship->x1;
+        ship->missiles->y1 = ship->y1;
+        ship->missiles->x2 = ship->x1;
+        ship->missiles->y2 = ship->y1 - MIS_LEN;
+        ship->missiles->vx = 0;
+        ship->missiles->vy = -MIS_VEL;
+        ship->missiles->next = NULL;
+    }
+}
+
+void MissileFired(Missile *missile)
+{
+    missile->x1 += missile->vx;
+    missile->y1 += missile->vy;
+    missile->x2 += missile->vx;
+    missile->y2 += missile->vy;
+}
+
+void MissileFliedAway(Missile *missiles)
+{
+    Missile *ixMissile = missiles;
+    if (ixMissile) {
+        Missile *auxMissile;
+        while (ixMissile->next != NULL) {
+            if (ixMissile->next->y1 < 0) {
+                auxMissile = ixMissile->next->next;
+                SDL_free(ixMissile->next);
+                ixMissile->next = auxMissile;
+            } else {
+                ixMissile = ixMissile->next;
+            }
+        }
+    }
 }
