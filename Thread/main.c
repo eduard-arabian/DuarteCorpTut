43a4bfb007a3486ecc3a78c5c9a7e4641089f8b6
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-21 15:35:18 +0300

    Thread

---
+++ Thread/main.c
@@ -0,0 +1,27 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *ProcessSeparate(void *data);
+
+int main()
+{
+    pthread_t process1;
+    pthread_t process2;
+
+    pthread_create(&process1, NULL, &ProcessSeparate, "Hi");
+    pthread_create(&process2, NULL, &ProcessSeparate, "Bye");
+    pthread_join(process1, NULL);
+    pthread_join(process2, NULL);
+
+    return 0;
+}
+
+void *ProcessSeparate(void *data)
+{
+    char *text = (char *) data;
+
+    while (1) {
+        printf("%s\n", text);
+    }
+}
