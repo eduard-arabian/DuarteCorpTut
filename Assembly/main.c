1ded41fe0cb8758fb239d8566b3d346e1cfd2b33
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-25 12:58:43 +0300

    Assembly

--- Assembly/main.c
+++ Assembly/main.c
@@ -3,10 +3,10 @@
 int main(void)
 {
     int a = 0;
-    asm("movl %1, %0\n"
-        : "=g"(a) // salida
-        : "g"(5)  // entrada
-        :         // clobbers
+    __asm__("movl %1, %0\n"
+            : "=g"(a) // salida
+            : "g"(5)  // entrada
+            :         // clobbers
     );
     printf("a: %d\n", a);
     return 0;
