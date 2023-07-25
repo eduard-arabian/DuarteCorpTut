3dbea4d5d617488ca61d0baf473d2a3f2ab9d489
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-25 13:43:41 +0300

    Assembly

--- Assembly/main.c
+++ Assembly/main.c
@@ -26,7 +26,7 @@ int main(void) {
       : "%eax");
   printf("a: %d\n", a);
 
-  // task several variables
+  // task several variables, opt 1
   a = 0;
   int b = 0;
   int c = 0;
@@ -42,6 +42,26 @@ int main(void) {
       : [value_a] "g"(1), [value_b] "g"(2), [value_c] "g"(3), [value_d] "g"(4),
         [value_e] "g"(5)
       :);
+  printf("a = %d\n", a);
+  printf("b = %d\n", b);
+  printf("c = %d\n", c);
+  printf("d = %d\n", d);
+  printf("e = %d\n", e);
+
+  // task several variables, opt 2
+  a = 0;
+  b = 0;
+  c = 0;
+  d = 0;
+  e = 0;
+  __asm__("movl %5, %0\n"
+          "movl %6, %1\n"
+          "movl %7, %2\n"
+          "movl %8, %3\n"
+          "movl %9, %4\n"
+          : "=g"(a), "=g"(b), "=g"(c), "=g"(d), "=g"(e)
+          : "g"(1), "g"(2), "g"(3), "g"(4), "g"(5)
+          :);
   printf("a = %d\n", a);
   printf("b = %d\n", b);
   printf("c = %d\n", c);
