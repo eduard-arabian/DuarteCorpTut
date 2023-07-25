6c55ffde6b733eff1c01cc1d48a66ddadfe72e53
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-25 13:07:49 +0300

    Assembly

--- Assembly/main.c
+++ Assembly/main.c
@@ -17,5 +17,14 @@ int main(void) {
           :);
 
   printf("a: %d\n", a);
+
+  // option 3
+  __asm__("movl %1,%%eax\n"
+          "movl %%eax,%0\n"
+          : "=g"(a)
+          : "g"(15)
+          : "%eax");
+
+  printf("a: %d\n", a);
   return 0;
 }
