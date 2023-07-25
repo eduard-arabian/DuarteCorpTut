e78f55f239dab6dde872acbc77cb22082f6739a0
Eduard Arabian <eduard.arabian@gmail.com>
2023-07-25 13:04:26 +0300

    Assembly

--- Assembly/main.c
+++ Assembly/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int a = 0;
-    // option 1
-    __asm__("movl %1, %0\n"
-            : "=g"(a) // salida, %0 = a
-            : "g"(5)  // entrada, %1 = 5
-            :         // clobbers
-    );
-    printf("a: %d\n", a);
+int main(void) {
+  int a = 0;
+  // option 1
+  __asm__("movl %1, %0\n"
+          : "=g"(a)  // salida, %0 = a
+          : "g"(5)   // entrada, %1 = 5
+          :          // clobbers
+  );
+  printf("a: %d\n", a);
 
-    // option 2
-    __asm__("movl %[value_of_a], %[variable_a]\n" : [variable_a] "=g"(a) : [value_of_a] "g"(7) :);
+  // option 2
+  __asm__("movl %[value_of_a], %[variable_a]\n"
+          : [variable_a] "=g"(a)
+          : [value_of_a] "g"(7)
+          :);
 
-    printf("a: %d\n", a);
-    return 0;
+  printf("a: %d\n", a);
+  return 0;
 }
